0bad902518af8ee5fb15313e514bac1a7e53ebd5
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-09-07 17:15:18 +0900

    Initial Commit

---
+++ onnseiapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onnseiapp
+//
+//  Created by user on 2020/09/07.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
